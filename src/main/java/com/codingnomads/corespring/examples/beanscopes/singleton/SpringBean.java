86f6a624d48e62c73c7e10444212db872950ce66
Chris Parker <chris@parkerdesignbuild.com>
2024-04-19 05:21:32 -0400

    update

--- src/main/java/com/codingnomads/corespring/examples/beanscopes/singleton/SpringBean.java
+++ src/main/java/com/codingnomads/corespring/examples/beanscopes/singleton/SpringBean.java
@@ -3,6 +3,6 @@ package com.codingnomads.corespring.examples.beanscopes.singleton;
 
 public class SpringBean {
     public SpringBean() {
-        System.out.println("-----Hey, I'm a Spring bean, not a String bean!-----");
+        System.out.println("-----Hey, I'm a Spring bean, not a String Bean!-----");
     }
 }
