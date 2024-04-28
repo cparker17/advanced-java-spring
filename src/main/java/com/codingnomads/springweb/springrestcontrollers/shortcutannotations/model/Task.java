8c79c807b6a38c45d66ca816db8ea8836288f6d1
Chris Parker <chris@parkerdesignbuild.com>
2024-04-28 08:40:37 -0400

    controller demo lab

--- src/main/java/com/codingnomads/springweb/springrestcontrollers/shortcutannotations/model/Task.java
+++ src/main/java/com/codingnomads/springweb/springrestcontrollers/shortcutannotations/model/Task.java
@@ -2,10 +2,7 @@
 package com.codingnomads.springweb.springrestcontrollers.shortcutannotations.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
--- src/main/java/com/codingnomads/springweb/springrestcontrollers/simpledemo/controller/HelloWorldController.java
+++ src/main/java/com/codingnomads/springweb/springrestcontrollers/simpledemo/controller/HelloWorldController.java
@@ -1,8 +1,12 @@
 /* CodingNomads (C)2024 */
 package com.codingnomads.springweb.springrestcontrollers.simpledemo.controller;
 
+import com.codingnomads.springweb.springrestcontrollers.shortcutannotations.model.Task;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -16,5 +20,31 @@ public class HelloWorldController {
     @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String greeting(@PathVariable(name = "name") String name) {
         return "Hello " + name + "!";
+    }
+
+    @RequestMapping(path = "/list", method = RequestMethod.GET)
+    public String list() {
+        Task task1 = new Task();
+        Task task2 = new Task();
+        Task task3 = new Task();
+
+        task1.setId(1L);
+        task1.setName("labs");
+        task1.setCompleted(true);
+
+        task2.setId(2L);
+        task2.setName("project");
+        task2.setCompleted(true);
+
+        task3.setId(3L);
+        task3.setName("exam");
+        task3.setCompleted(false);
+
+        ArrayList<Task> taskList = new ArrayList<>();
+        taskList.add(task1);
+        taskList.add(task2);
+        taskList.add(task3);
+
+        return taskList.toString();
     }
 }
